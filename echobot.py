9090b53aad51734c069b54c749d2923e083c6250
Lucas-Armand <oliveira.armand@poli.ufrj.br>
2017-01-16 20:40:13 -0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -36,5 +36,15 @@ def send_message(text, chat_id):
     get_url(url)
 
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
