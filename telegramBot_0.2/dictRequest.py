91746853f6836b189ece0f1958395c4920406cf7
Lucas-Armand <oliveira.armand@poli.ufrj.br>
2017-01-26 03:29:56 -0200

    Create 'dictTime'/'dictDate' and make dictKeys = list()

---
+++ telegramBot_0.2/dictRequest.py
@@ -0,0 +1,36 @@
+
+schedule ={
+    'horário':'schedule',
+    'horario':'schedule',
+    'marcar':'schedule',
+    'horários':'schedule'
+
+}
+
+reschedule = {
+    'remarcar':'reschedule',
+    'adiar':'reschedule',
+    'outro':'reschedule',
+    'diferente':'reschedule'
+
+    }
+
+cancelation ={
+    'cancelar':'cancelation',
+    'excluir':'cancelation',
+    'infelizmente':'cancelation'
+    }
+
+information = {
+    'informação':'information',
+    'preço':'information',
+    'informações':'information',
+    'serviços':'information',
+    'tipos':'information',
+    'atendimento':'information',
+    'funciona':'information'
+
+    }
+
+
+dictKeys = list(schedule.keys())+list(reschedule.keys())+list(cancelation.keys())+list(information.keys())
---
+++ telegramBot_0.2/dictTime.py
@@ -0,0 +1,8 @@
+time = {
+    'manhã':'10:00',
+    'tarde':'15:00',
+    'noite':'19:00',
+    'agora':'*'
+    }
+
+dictKeys = list(time.keys())
