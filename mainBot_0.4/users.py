4f11089db295385ec5f990813dede9b2ca8424ee
Unknown <oliveira.armand@poli.ufrj.br>
2017-02-28 15:24:44 -0300

    Revert "main 0.4 com schedule funcionando"
    This reverts commit f8048a158a015fbe03703a6b18c19751d988cbf9.

--- mainBot_0.4/users.py
+++
@@ -1,9 +0,0 @@
-userTable = {
-        '318089574': {
-            'request': '',
-            'data': '',
-            'time': '',
-            'confirm': '',
-            'job': '',
-            'lastcall': ''}
-    }
